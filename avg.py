62240d59c1fd3f70f79ecc2ccb83281a1a1c5055
A B Keshav Kumar <ae24s021@smail.iitm.ac.in>
2026-02-03 16:16:40 +0530

    Implement average_of_five function

--- avg.py
+++ avg.py
@@ -10,12 +10,8 @@
 #     nums = list(map(float, input().split()))
 #     print(average_of_five(nums))
 def average_of_five(nums):
-    """
-    nums: list of 5 numbers
-    return: average
-    """
-    # TODO: implement this function
-    pass
+    return sum(nums) / 5
+
 
 if __name__ == "__main__":
     nums = list(map(float, input().split()))
