0952f32a998dbcedbf1b532405fc534a86840cb4
A B Keshav Kumar <ae24s021@smail.iitm.ac.in>
2026-02-03 16:13:58 +0530

    Uncomment average_of_five function and call

--- avg.py
+++ avg.py
@@ -1,3 +1,14 @@
+# def average_of_five(nums):
+#     """
+#     nums: list of 5 numbers
+#     return: average
+#     """
+#     # TODO: implement this function
+#     pass
+
+# if __name__ == "__main__":
+#     nums = list(map(float, input().split()))
+#     print(average_of_five(nums))
 def average_of_five(nums):
     """
     nums: list of 5 numbers
