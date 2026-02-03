fe89ed369129c80ab34013045f8d03c2097c2547
A B Keshav Kumar <ae24s021@smail.iitm.ac.in>
2026-02-03 16:10:42 +0530

    Update and rename test_avg.py to tests/test_avg.py

--- tests/test_avg.py
+++ tests/test_avg.py
@@ -1,10 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from avg import average_of_five
+
 
 def test_simple():
     assert average_of_five([1, 2, 3, 4, 5]) == 3.0
 
+
 def test_all_same():
     assert average_of_five([2, 2, 2, 2, 2]) == 2.0
+
 
 def test_negative():
     assert average_of_five([-1, -2, -3, -4, -5]) == -3.0
