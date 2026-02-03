74bcd8d1e50baed1c5d14b14cb245abb63d9d01a
A B Keshav Kumar <ae24s021@smail.iitm.ac.in>
2026-02-03 15:56:22 +0530

    Add unit tests for average_of_five function

---
+++ test_avg.py
@@ -0,0 +1,10 @@
+from avg import average_of_five
+
+def test_simple():
+    assert average_of_five([1, 2, 3, 4, 5]) == 3.0
+
+def test_all_same():
+    assert average_of_five([2, 2, 2, 2, 2]) == 2.0
+
+def test_negative():
+    assert average_of_five([-1, -2, -3, -4, -5]) == -3.0
